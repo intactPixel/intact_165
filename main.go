9f6339060ad0376b01ae50de80cf79f63ba565da
Intact Pixel <intact.pixel@mail.ru>
2025-01-30 17:06:38 +0300

    tasks

--- main.go
+++ main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"log"
+	"net/http"
 )
 
-func main() {
-	a := []int{}
-	for i := 0; i < 20; i++ {
-		a = append(a, rand.Intn(1000)+100)
-	}
-	fmt.Println("unsorted array:", a)
-	bubbleSort(a)
-	fmt.Println("sorted array:", a)
-	fmt.Println(a)
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Устанавливаем заголовок Content-Type
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
+	// Открываем HTML-файл
+	http.ServeFile(w, r, "index.html")
+}
+
+func main() {
+	//a := []int{}
+	//for i := 0; i < 20; i++ {
+	//	a = append(a, rand.Intn(1000)+100)
+	//}
+	//fmt.Println("unsorted array:\n", a)
+	//bubbleSort(a)
+	//fmt.Println("sorted array:", a)
+	//fmt.Println(a)
+	//
+	//for _, i := range a {
+	//	fmt.Printf("%d ", i)
+	//}
+	// handler функция для обработки HTTP-запросов
+
+	// Устанавливаем обработчик для корневого маршрута
+	http.HandleFunc("/", handler)
+
+	// Запускаем сервер на порту 8080
+	log.Println("Сервер запущен на http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Ошибка при запуске сервера:", err)
+	}
 }
 
 func bubbleSort(array []int) {
